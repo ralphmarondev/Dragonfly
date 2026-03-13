f5dea9b228eb0fefd4d5dd3eff3fc3abfa40b4ee
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-03-13 11:57:16 +0800

    update auth with firebase

--- app/src/main/java/com/ralphmarondev/dragonfly/core/di/CoreModule.kt
+++ app/src/main/java/com/ralphmarondev/dragonfly/core/di/CoreModule.kt
@@ -1,9 +1,13 @@
 package com.ralphmarondev.dragonfly.core.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ralphmarondev.dragonfly.core.data.local.preferences.AppPreferences
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val coreModule = module {
     single { AppPreferences(context = androidContext().applicationContext) }
+    single { FirebaseAuth.getInstance() }
+    single { FirebaseFirestore.getInstance() }
 }
--- app/src/main/java/com/ralphmarondev/dragonfly/features/auth/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/ralphmarondev/dragonfly/features/auth/data/repository/AuthRepositoryImpl.kt
@@ -1,13 +1,29 @@
 package com.ralphmarondev.dragonfly.features.auth.data.repository
 
+import com.google.firebase.auth.FirebaseAuth
+import com.ralphmarondev.dragonfly.core.data.local.preferences.AppPreferences
 import com.ralphmarondev.dragonfly.features.auth.domain.repository.AuthRepository
+import kotlinx.coroutines.tasks.await
 
-class AuthRepositoryImpl : AuthRepository {
+class AuthRepositoryImpl(
+    private val auth: FirebaseAuth,
+    private val preferences: AppPreferences
+) : AuthRepository {
     override suspend fun login(email: String, password: String): Boolean {
-        return email == "ralphmaron@cute.com" && password == "super.cute"
+        val authResult = auth.signInWithEmailAndPassword(email, password).await()
+        authResult.user ?: return false
+
+        preferences.setAuthenticated(true)
+        preferences.setEmail(email)
+        return true
     }
 
     override suspend fun register(email: String, password: String): Boolean {
+        val authResult = auth.createUserWithEmailAndPassword(email, password).await()
+        authResult.user ?: return false
+
+        preferences.setAuthenticated(true)
+        preferences.setEmail(email)
         return true
     }
 }
