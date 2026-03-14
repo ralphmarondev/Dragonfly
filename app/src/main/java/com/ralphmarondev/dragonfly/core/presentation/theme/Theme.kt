3ec5ecf8688493242f366ca313262e68476c136c
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-03-14 14:22:36 +0800

    update theme

--- app/src/main/java/com/ralphmarondev/dragonfly/core/presentation/theme/Theme.kt
+++ app/src/main/java/com/ralphmarondev/dragonfly/core/presentation/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.ralphmarondev.dragonfly.core.presentation.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -20,16 +24,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -47,6 +41,17 @@ fun DragonflyTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insetsController = window?.let {
+                WindowCompat.getInsetsController(it, view)
+            }
+            insetsController?.isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
