3376c2fa012a6570372ae9c315df3d2559f112e5
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-03-23 11:22:06 +0800

    add floating action button to show notification

--- app/src/main/java/com/ralphmarondev/dragonfly/features/dashboard/presentation/DashboardScreen.kt
+++ app/src/main/java/com/ralphmarondev/dragonfly/features/dashboard/presentation/DashboardScreen.kt
@@ -1,5 +1,10 @@
 package com.ralphmarondev.dragonfly.features.dashboard.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,7 +16,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Fireplace
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -25,10 +33,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import com.ralphmarondev.dragonfly.core.common.NotificationHelper
 import com.ralphmarondev.dragonfly.core.domain.model.Location
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -58,6 +72,25 @@ private fun DashboardScreen(
     state: DashboardState,
     action: (DashboardAction) -> Unit
 ) {
+    val context = LocalContext.current
+
+    var permissionGranted by remember { mutableStateOf(false) }
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        permissionGranted = granted
+    }
+
+    LaunchedEffect(Unit) {
+        permissionGranted = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -78,7 +111,33 @@ private fun DashboardScreen(
                     titleContentColor = MaterialTheme.colorScheme.onPrimary
                 )
             )
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    // NOTE: THIS SHOULD BE DONE ON THE BACKGROUND WHEN THE LOCATION OF THE
+                    // VEHICLE IS CHANGED.
+                    if (permissionGranted) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                        }
+                    } else {
+                        NotificationHelper.sendNotification(
+                            context = context,
+                            id = 1,
+                            title = "Dragonfly Notification",
+                            content = "Your vehicle is moved. Lolz :)"
+                        )
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Fireplace,
+                    contentDescription = null
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { innerPadding ->
         PullToRefreshBox(
             isRefreshing = state.isRefreshing,
